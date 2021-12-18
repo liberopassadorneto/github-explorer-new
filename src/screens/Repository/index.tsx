d4b8b02f80d44fa6fc2e975a153273dd9818d278
Libero Passador Neto <libero@griks.com.br>
2021-12-17 20:32:24 -0400

    first commit

---
+++ src/screens/Repository/index.tsx
@@ -0,0 +1,95 @@
+import { useRoute } from '@react-navigation/core';
+import React from 'react';
+import { Linking } from 'react-native';
+import { Background } from '../../components/Background';
+import { Card } from '../../components/Card';
+import { useRepositories } from '../../hooks/useRepositories';
+import {
+  Container,
+  Description,
+  Forks,
+  ForksCounter,
+  ForksText,
+  IssuesList,
+  OpenIssues,
+  OpenIssuesCounter,
+  OpenIssuesText,
+  OwnerAvatar,
+  RepoInfo,
+  RepoStats,
+  Stars,
+  StarsCounter,
+  StarsText,
+  TextGroup,
+} from './styles';
+import { TitleAnimation } from './TitleAnimation';
+
+interface RepositoryParams {
+  repositoryId: number;
+}
+
+export function Repository() {
+  const { params } = useRoute();
+  const { repositoryId } = params as RepositoryParams;
+  const { findRepositoryById } = useRepositories();
+  const repository = findRepositoryById(repositoryId);
+
+  function handleIssueNavigation(issueUrl: string) {
+    Linking.openURL(issueUrl);
+  }
+
+  return (
+    <Background>
+      <Container>
+        <RepoInfo>
+          <OwnerAvatar source={{ uri: repository.owner.avatar_url }} />
+
+          <TextGroup>
+            <TitleAnimation>{repository.full_name}</TitleAnimation>
+
+            <Description numberOfLines={2}>
+              {repository.description}
+            </Description>
+          </TextGroup>
+        </RepoInfo>
+
+        <RepoStats>
+          <Stars>
+            <StarsCounter>{repository.stargazers_count}</StarsCounter>
+            <StarsText>Stars</StarsText>
+          </Stars>
+
+          <Forks>
+            <ForksCounter>{repository.forks_count}</ForksCounter>
+            <ForksText>Forks</ForksText>
+          </Forks>
+
+          <OpenIssues>
+            <OpenIssuesCounter>
+              {repository.open_issues_count}
+            </OpenIssuesCounter>
+            <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
+          </OpenIssues>
+        </RepoStats>
+
+        <IssuesList
+          data={repository.issues}
+          keyExtractor={(issue) => String(issue.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: issue }) => (
+            <Card
+              data={{
+                id: issue.id,
+                title: issue.title,
+                subTitle: issue.user.login,
+              }}
+              onPress={() => {
+                handleIssueNavigation(issue.html_url);
+              }}
+            />
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
